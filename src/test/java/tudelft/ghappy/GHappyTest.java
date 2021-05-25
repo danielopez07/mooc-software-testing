d0970fbd25dd03da72df784891c9d7bd9c9ddfde
Daniel Antonio Lopez Delgado <danielopez07@gmail.com>
2021-05-24 22:12:29 -0300

    Quiz: Topics 1 & 2, G Happy

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,25 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+    @ParameterizedTest(name = "{0}: ({1}) = {2}")
+    @CsvSource({
+            // no string
+            "'no-string','',false",
+            // one character string
+            "'one-char-string','g',false",
+            // two characters string true
+            "'two-chars-true','gg',true",
+            // two characters string false
+            "'two-chars-false','ag',false",
+            // three characters false
+            "'three-chars','gag',false",
+    })
+    public void testAlgorithm(String partition, String string, boolean expectedResult) {
+        boolean result = new GHappy().gHappy(string);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
