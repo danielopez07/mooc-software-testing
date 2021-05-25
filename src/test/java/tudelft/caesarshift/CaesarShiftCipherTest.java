e0185f3052590a1bf90eb21e620a2670b3c67fd7
Daniel Antonio Lopez Delgado <danielopez07@gmail.com>
2021-05-24 22:53:40 -0300

    Quiz: Topics 1 & 2, Caesar Cipher

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,19 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+public class CaesarShiftCipherTest {
+    @ParameterizedTest(name = "{0}: {1}, {2} = {3}")
+    @CsvSource({
+            // One character, not passing through a or z
+            "'one-char-not-passing-a-or-z', 'a', '2', 'c'", "'one-char-not-passing-a-or-z', 'k', '2', 'm'",
+            // Multiple characters
+            "'multiple-characters', 'abc', '2', 'cde'"
+    })
+    public void testAlgorithm(String partition, String message, int shift, String expectedResult) {
+        String result = new CaesarShiftCipher().CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
